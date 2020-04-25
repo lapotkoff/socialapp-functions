d1db1700121a4d8cebb1116ad052ea1a091c0369
lapotkoff <valentyn.lapotkov@agiliway.com>
2020-04-25 16:26:42 +0300

    initial commit of firebase cloud functions for my social app: added initial login/signup with validation and some other related stuff

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+        userHandle: 'user',
+        body: 'this is a scream body',
+        createdAt: '2020-04-25T09:33:40.187Z',
+        likeCount: 5,
+        commentCount: 2,
+    }
+  ]
+};
