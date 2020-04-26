567d3a20dd569c6deadaf827bb1bf7f8ac5d0c76
lapotkoff <valentyn.lapotkov@agiliway.com>
2020-04-26 17:59:53 +0300

    added GET and POST user data functionality

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -5,7 +5,7 @@ const config = require('../util/config');
 const firebase = require('firebase');
 firebase.initializeApp(config);
 
-const { validateSignupData, validateLoginData } = require('../util/validators');
+const { validateSignupData, validateLoginData, reduceUserDetails } = require('../util/validators');
 
 exports.signup = (req, res) => {
   const newUser = {
@@ -55,7 +55,7 @@ exports.signup = (req, res) => {
         return res.status(500).json({ error: err.code });
       }
     })
-}
+};
 
 exports.login = (req, res) => {
   const user = {
@@ -81,8 +81,44 @@ exports.login = (req, res) => {
         return res.status(500).json({ error: err.code });
       }
     })
-}
+};
 
+exports.addUserDetails = (req, res) => {
+  let userDetails = reduceUserDetails(req.body);
+
+  db.doc(`/users/${req.user.handle}`).update(userDetails)
+    .then(() => {
+      return res.json({ message: 'Details added successfully!' });
+    })
+    .catch(err => {
+      console.error('Error while updating user data: ', err);
+      return res.status(500).json({ error: err.code });
+    })
+};
+
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.user.handle}`).get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db.collection('likes').where('userHandle', '==', req.user.handle).get();
+      }
+    })
+    .then(data => {
+      userData.likes = [];
+      data.forEach(doc => {
+        userData.likes.push(doc.data());
+      });
+      return res.json(userData);
+    })
+    .catch(err => {
+      console.error('Error while getting user data: ', err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+// Upload a profile image
 exports.uploadImage = (req, res) => {
   const BusBoy = require('busboy');
   const path = require('path');
